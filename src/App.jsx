24dbd0b20cc56d7e3488bb1361f5b94f1a555413
Abdosaber123 <blaza.cf@gmail.com>
2026-02-05 22:16:39 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './component/Layout.jsx'
 import Home from './component/Home.jsx'
--- src/component/Video.jsx
+++ src/component/Video.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import img1 from "../assets/1000013280.jpg"
-import img2 from "../assets/1a8d37f4d96d48a22d8f6f1b1dc54043.jpg"
-import img3 from "../assets/6ca4557be13c131270f1f8852ac1e511_t.jpeg"
-import img4 from "../assets/dinner.jpg"
+import img1 from "../assets/111111.jpeg";
+import img2 from "../assets/22222222222.jpeg";
+import img3 from "../assets/333333333.jpeg";
+import img4 from "../assets/44444444444444.jpeg";
+import img5 from "../assets/5555555555555555555555.jpeg";
+import img6 from "../assets/6666666666666666666666.jpeg";
+import img7 from "../assets/777777777777777777777777.jpeg";
+import img8 from "../assets/88888888888888888.jpeg";
+import img9 from "../assets/99999999999999.jpeg";
+
 export default function Video() {
   return (
     <div className="bg-[#FAA9BC] py-4 px-9">
       <div className=" flex justify-center mt-4 px-3">
         <iframe
-        className=""
           width="560"
-          height="215"
-          src="https://www.youtube.com/embed/NYntUyksG5E?si=bCfcqox0r4uktCK0"
+          height="315"
+          src="https://www.youtube.com/embed/GvnKzzY1x34?si=8_ryfjir38ugGFUv"
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -21,44 +26,71 @@ export default function Video() {
       </div>
       <div className="images">
         <div className="1 bg-white w-fit mx-auto rounded-xl flex flex-col  items-center mt-4">
-            <img src={img1} className="w-[300px]" alt="" srcset="" />
-            <p className="mt-3 text-pink-400 font-bold py-2 ">ربنا يخليكى ليا</p>
+          <img src={img1} className="w-[300px]" alt="" srcset="" />
+          <p className="mt-3 text-pink-400 font-bold py-2 "> بحبك </p>
         </div>
         <div className="1 bg-white w-fit mx-auto rounded-xl flex flex-col  items-center mt-4">
-            <img src={img2} className="w-[300px]" alt="" srcset="" />
-            <p className="mt-3 text-pink-400 font-bold py-2 ">احسن يوم</p>
+          <img src={img2} className="w-[300px]" alt="" srcset="" />
+          <p className="mt-3 text-pink-400 font-bold py-2 ">احسن يوم</p>
         </div>
         <div className="1 bg-white w-fit mx-auto rounded-xl flex flex-col  items-center mt-4">
-            <img src={img3} className="w-[300px]" alt="" srcset="" />
-            <p className="mt-3 text-pink-400 font-bold py-2 ">بحبك</p>
+          <img src={img3} className="w-[300px]" alt="" srcset="" />
+          <p className="mt-3 text-pink-400 font-bold py-2 ">ربنا يخليكى ليا</p>
         </div>
         <div className="1 bg-white w-fit mx-auto rounded-xl flex flex-col  items-center mt-4">
-            <img src={img4} className="w-[300px]" alt="" srcset="" />
-            <p className="mt-3 text-pink-400 font-bold py-2 ">🫶🏻 💕</p>
+          <img src={img4} className="w-[300px]" alt="" srcset="" />
+          <p className="mt-3 text-pink-400 font-bold py-2 ">🫶🏻 💕</p>
+        </div>
+        <div className="1 bg-white w-fit mx-auto rounded-xl flex flex-col  items-center mt-4">
+          <img src={img5} className="w-[300px]" alt="" srcset="" />
+          <p className="mt-3 text-pink-400 font-bold py-2 ">💖</p>
+        </div>
+        <div className="1 bg-white w-fit mx-auto rounded-xl flex flex-col  items-center mt-4">
+          <img src={img6} className="w-[300px]" alt="" srcset="" />
+          <p className="mt-3 text-pink-400 font-bold py-2 ">🫶🏻 💋</p>
+        </div>
+        <div className="1 bg-white w-fit mx-auto rounded-xl flex flex-col  items-center mt-4">
+          <img src={img7} className="w-[300px]" alt="" srcset="" />
+          <p className="mt-3 text-pink-400 font-bold py-2 ">❤️‍🔥</p>
+        </div>
+        <div className="1 bg-white w-fit mx-auto rounded-xl flex flex-col  items-center mt-4">
+          <img src={img8} className="w-[300px]" alt="" srcset="" />
+          <p className="mt-3 text-pink-400 font-bold py-2 ">🫶🏻 💕</p>
+        </div>
+        <div className="1 bg-white w-fit mx-auto rounded-xl flex flex-col  items-center mt-4">
+          <img src={img9} className="w-[300px]" alt="" srcset="" />
+          <p className="mt-3 text-pink-400 font-bold py-2 ">💘</p>
         </div>
       </div>
       <div className="time bg-white px-3 py-7 mt-4 rounded-xl shadow-lg">
-        <h2 className="text-center text-pink-500 font-semibold">الوقت الى فات معاك</h2>
+        <h2 className="text-center text-pink-500 font-semibold">
+          الوقت الى فات معاك
+        </h2>
         <div className="group flex gap-3 flex-wrap px-3 mt-3">
-            <div className="1 bg-pink-500 py-3 rounded-xl text-white w-[47%] text-center">
-                <p className="text-3xl font-semibold mb-3">18</p>
-                <p className="text-xl font-bold">ساعة</p>
-            </div>
-             <div className="2 bg-pink-500 py-3 rounded-xl text-white w-[47%] text-center ">
-                <p className="text-3xl font-semibold mb-3">551</p>
-                <p className="text-xl font-bold">يوم</p>
-            </div>
-            <div className="3 bg-pink-500 py-3 rounded-xl text-white w-[47%] text-center">
-                <p className="text-3xl font-semibold mb-3">20</p>
-                <p className="text-xl font-bold">ثانية</p>
-            </div>
-             <div className="4 bg-pink-500 py-3 rounded-xl text-white w-[47%] text-center ">
-                <p className="text-3xl font-semibold mb-3">19</p>
-                <p className="text-xl font-bold">دقيقة</p>
-            </div>
+          <div className="1 bg-pink-500 py-3 rounded-xl text-white w-[47%] text-center">
+            <p className="text-3xl font-semibold mb-3">3,600</p>
+            <p className="text-xl font-bold">ساعة</p>
+          </div>
+          <div className="2 bg-pink-500 py-3 rounded-xl text-white w-[47%] text-center ">
+            <p className="text-3xl font-semibold mb-3">150</p>
+            <p className="text-xl font-bold">يوم</p>
+          </div>
+          <div className="3 bg-pink-500 py-3 rounded-xl text-white w-[47%] text-center">
+            <p className="text-3xl font-semibold mb-3">12,960,000</p>
+            <p className="text-xl font-bold">ثانية</p>
+          </div>
+          <div className="4 bg-pink-500 py-3 rounded-xl text-white w-[47%] text-center ">
+            <p className="text-3xl font-semibold mb-3">216,000</p>
+            <p className="text-xl font-bold">دقيقة</p>
+          </div>
         </div>
-        <p className="text-center mt-3 font-semibold text-xl text-pink-500">  💖 سوا من 3 / 8 / 2024</p>
-        <p className="text-red-200 mt-3 rounded-2xl py-2  bg-red-500 text-center text-3xl font-semibold">بحبك</p>
+        <p className="text-center mt-3 font-semibold text-xl text-pink-500">
+          {" "}
+          💖 سوا من 3 / 8 / 2024
+        </p>
+        <p className="text-red-200 mt-3 rounded-2xl py-2  bg-red-500 text-center text-3xl font-semibold">
+          بحبك
+        </p>
       </div>
     </div>
   );
