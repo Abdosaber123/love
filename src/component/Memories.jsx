b34b25b2659756c4031b4a469c06d38a6a5c335a
Abdosaber123 <blaza.cf@gmail.com>
2026-02-05 17:14:54 +0200

    first commit

--- src/component/Memories.jsx
+++ src/component/Memories.jsx
@@ -5,7 +5,7 @@ import { TypeAnimation } from "react-type-animation";
 export default function Memories() {
     const [showBtn , setShowBtn] = useState(false)
   return (
-    <>
+    <div className='bg-pink-400 h-screen text-white'>
     <h2 className='text-center pt-4 text-4xl font-bold mb-7'> 🫵🏻💕ينور عينى</h2>
     <div className='text-right py-4 px-3 leading-10 font-semibold text-xl'>
        <TypeAnimation
@@ -30,11 +30,11 @@ export default function Memories() {
         style={{ whiteSpace: "pre-line" }} // عشان السطور
       />
     <div className="flex justify-center">
-              <Link to={"/love"} className={` transition-all duration-300 mt-5 bg-[#F4C3BD] px-16  py-3 ${showBtn? "translate-y-0 opacity-100" :"translate-y-16 opacity-0"}`} type="button"> ابدأ الرحلة </Link>
+              <Link to={"/love"} className={` transition-all duration-300 mt-5 rounded-xl bg-pink-400 border border-black px-16  py-3 ${showBtn? "translate-y-0 opacity-100" :"translate-y-16 opacity-0"}`} type="button"> ابدأ الرحلة </Link>
 
     </div>
     </div>
-    </>
+    </div>
   )
   
 }
