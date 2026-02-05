b245b98c07d52add00c0d80d1aaa459b467667e4
Abdosaber123 <blaza.cf@gmail.com>
2026-02-05 22:17:56 +0200

    first commit d

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     const [pass , setPass] = useState("")
     const nav = useNavigate()
     function acceptPass() {
-        if(pass != "love"){
+        if(pass != "BestFriendForEver"){
             window.alert("Check Your Pass My Love ♥️")
             
         }else{
